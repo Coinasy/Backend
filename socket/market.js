afec5b1f101a3f0cd080ae402c8b184be7b51a0a
leangseu <leangseu_kim@student.uml.edu>
2018-01-01 18:44:00 -0500

    change socket for market upate

--- socket/market.js
+++ socket/market.js
@@ -2,13 +2,29 @@ module.exports = function(io) {
     const axios = require('axios')
     io.sockets.on('connection', function(socket) {
         console.log('socket is connected')
-        // emit axios every 1 sec
+        // emit axios every 10 sec
         setInterval(function(){
             axios.get('https://api.coinmarketcap.com/v1/ticker/?limit=0').then(data => {
-                socket.emit('market', data.data);
+                socket.emit('all market respond', data.data);
             }).catch(err => {
                 console.log(err)
             })
-        }, 1000);
+        }, 10000);
+
+        socket.on('all market request', () => {
+            axios.get('https://api.coinmarketcap.com/v1/ticker/?limit=0').then(data => {
+                socket.emit('all market respond', data.data);
+            }).catch(err => {
+                console.log(err)
+            })
+        })
+
+        socket.on('coin id request', function(data) {
+            axios.get('https://api.coinmarketcap.com/v1/ticker/' + data.id).then(data => {
+                socket.emit('coin id respond', data.data)
+            }).catch(err => {
+                console.log(err)
+            })
+        })
     })
 }
